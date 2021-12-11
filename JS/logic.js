6ba570060fb6cecd75ba1cb373eee2bdbf862366
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2021-12-11 08:40:04 +0800

    filter in progress

--- JS/logic.js
+++ JS/logic.js
@@ -1,56 +1,90 @@
-check = () => {
-  if (
-    !document
-      .getElementById("dropdownMenuButton")
-      .getAttribute("class")
-      .split("")
-      .includes("visible")
-  ) {
-    dropChild = document.getElementById("dropdownMenuButton").children;
-    for (i in dropChild) {
-      myLevel = dropChild[i].getAttribute("class").split(" ");
-      if (myLevel.includes("active")) {
-        console.log(dropChild[i].getAttribute("value"));
-        return dropChild[i].getAttribute("value");
-      }
-      // if (dropChild[i])
+filter = () => {
+  let dropChild = document.getElementById("dropdownMenuButton").children;
+  for (i in dropChild) {
+    myLevel = dropChild[i].getAttribute("class").split(" ");
+    if (myLevel.includes("active")) {
+      console.log(dropChild[i].getAttribute("value"));
+      break;
     }
   }
-};
-
-check2 = () => {
-  if (check() in levelDict) {
-    console.log("hi");
-  }
-  if (
-    !document
-      .getElementById("dropdownMenuButton2")
-      .getAttribute("class")
-      .split("")
-      .includes("visible")
-  ) {
-    dropChild = document.getElementById("dropdownMenuButton2").children;
-    for (i in dropChild) {
-      myLevel = dropChild[i].getAttribute("class").split(" ");
-      if (myLevel.includes("active")) {
-        console.log(dropChild[i].getAttribute("value"));
-      }
-      // if (dropChild[i])
+  let dropChild2 = document.getElementById("dropdownMenuButton2").children;
+  for (i in dropChild2) {
+    myLevel = dropChild2[i].getAttribute("class").split(" ");
+    if (myLevel.includes("active")) {
+      console.log(dropChild2[i].getAttribute("value"));
+      break;
     }
   }
+  return;
 };
 
-fuck = () => {
-  console.log("I luv Kaleb");
+showBooks = (type) => {
+  if (type == "All") {
+    $.getJSON("../JSON/store.json", (data) => {
+      console.log(data);
+    });
+  }
+  document.getElementById("books");
 };
 
-document.getElementById("dropdownMenuButton").addEventListener("click", check);
-document
-  .getElementById("dropdownMenuButton2")
-  .addEventListener("click", check2);
+// check = () => {
+//   if (
+//     !document
+//       .getElementById("dropdownMenuButton")
+//       .getAttribute("class")
+//       .split("")
+//       .includes("visible")
+//   ) {
+//     dropChild = document.getElementById("dropdownMenuButton").children;
+//     for (i in dropChild) {
+//       myLevel = dropChild[i].getAttribute("class").split(" ");
+//       if (myLevel.includes("active")) {
+//         console.log(dropChild[i].getAttribute("value"));
+//         return dropChild[i].getAttribute("value");
+//       }
+//       // if (dropChild[i])
+//     }
+//   }
+// };
 
-// document.getElementById("dropdownMenuButton").innerHTML = `
-// <div class="option active placeholder" data-value="placeholder">
-//   Select Level:
-// </div>
-// `
+// check2 = () => {
+//   if (check() in levelDict) {
+//     console.log("hi");
+//   }
+//   if (
+//     !document
+//       .getElementById("dropdownMenuButton2")
+//       .getAttribute("class")
+//       .split("")
+//       .includes("visible")
+//   ) {
+//     dropChild = document.getElementById("dropdownMenuButton2").children;
+// for (i in dropChild) {
+//   myLevel = dropChild[i].getAttribute("class").split(" ");
+//   if (myLevel.includes("active")) {
+//     console.log(dropChild[i].getAttribute("value"));
+//   }
+//   // if (dropChild[i])
+// }
+//   }
+// };
+
+// Remove Select Level From Drop Down
+document.getElementById("dropdownMenuButton").addEventListener("click", () => {
+  let dropChild = document.getElementById("dropdownMenuButton").children;
+  for (i in dropChild) {
+    place = dropChild[i].getAttribute("value");
+    if (place == "placeholder") {
+      dropChild[i].remove();
+    }
+  }
+});
+document.getElementById("dropdownMenuButton2").addEventListener("click", () => {
+  let dropChild = document.getElementById("dropdownMenuButton2").children;
+  for (i in dropChild) {
+    place = dropChild[i].getAttribute("value");
+    if (place == "placeholder") {
+      dropChild[i].remove();
+    }
+  }
+});
